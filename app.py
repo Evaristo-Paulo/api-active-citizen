e2e50e24f76498f4c58fedc53626153e3c76078f
Evaristo <evaripaulo@gmail.com>
2024-02-12 22:24:58 +0100

    creating function to add reports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date, time
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -25,21 +25,41 @@ class User(db.Model):
     password = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(30), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
-    reports = db.relationship('Report', backref='user', lazy=True)
+    #reports = db.relationship('Report', backref='user', lazy=True)
 
 
 class Report(db.Model):
     __tablename__ = 'reports'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     date = db.Column(db.Date, nullable=False)
     time = db.Column(db.Time, nullable=False)
     location = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+
+@app.route('/api/reports/add', methods=["POST"])
+def add_report():
+    data = request.json
+    # GENERATE DATE
+    str_date = data['date']
+    list_date = [int(item) for item in str_date.split('-')]
+    year, month, days = list_date
+    datetime_date = date(year, month, days)
+
+    # GENERATE TIME
+    str_time = data['time']
+    list_time = [int(item) for item in str_time.split(':')]
+    hours, min = list_time
+    datetime_time = time(hours, min, 0)
+
+    if 'date' in data and 'time' in data and 'location' in data and 'description' in data:
+        report = Report(date=datetime_date, time=datetime_time, location=data['location'], description=data['description'])
+        db.session.add(report)
+        db.session.commit()
+        return jsonify({"message": "Report stored successfully"})
+    return jsonify({"message": "Invalid report data"}), 400
+    
 
 
 if __name__ == '__main__':
